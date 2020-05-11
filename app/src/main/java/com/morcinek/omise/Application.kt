627c7cd7cdf7828332cbff41cc6c9a1881591bb9
Tomasz Morcinek <tomasz.morcinek@gmail.com>
2020-05-11 14:05:12 +0200

    Adding Retrofit (REST Api) to project.

--- app/src/main/java/com/morcinek/omise/Application.kt
+++ app/src/main/java/com/morcinek/omise/Application.kt
@@ -1,9 +1,16 @@
 package com.morcinek.omise
 
 import android.app.Application
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.scope.Scope
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class Application : Application() {
 
@@ -12,7 +19,28 @@ class Application : Application() {
         startKoin {
             androidLogger()
             androidContext(this@Application)
-            modules()
+            modules(
+                appModule
+            )
         }
     }
 }
+
+val appModule = module {
+
+    single<Retrofit> {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(
+                OkHttpClient.Builder().apply {
+                    if (BuildConfig.DEBUG) {
+                        addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+                    }
+                }.build()
+            )
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+            .build()
+    }
+}
+
+inline fun <reified T> Scope.getApi(): T = get<Retrofit>().create(T::class.java)
